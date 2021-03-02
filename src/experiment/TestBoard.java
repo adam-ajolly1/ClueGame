109ae0f08dce4c50cf3e267b82c1b727af7d56b9
Katherine Aubert <kaubert@mymail.mines.edu>
2021-03-01 22:24:47 -0700

    added data folder and tests for clueGame package for C13A-2

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -3,17 +3,42 @@ package experiment;
 import java.util.HashSet;
 
 public class TestBoard {
-	private TestBoardCell[][] board;
+	public final static int COLS = 4;
+	public final static int ROWS = 4;   
+	private TestBoardCell[][] board = new TestBoardCell[ROWS][COLS];
 	private HashSet<TestBoardCell> targets;
 	private HashSet<TestBoardCell> visited;
-	final static int COLS = 4;
-	final static int ROWS = 4;	
 	public TestBoard(TestBoardCell [][] arr) 
 	{
 		this.board = arr;
 	}
 	public void calcTargets( TestBoardCell startCell, int pathlength) {
-		
+		visited = new HashSet<TestBoardCell>();
+		targets = new HashSet<TestBoardCell>();
+		visited.add(startCell);
+		findAllTargets(startCell, pathlength);
+	}
+	public void findAllTargets(TestBoardCell thisCell, int numSteps) {
+		if (thisCell.isOccupied()) {
+			return;
+		}
+		if (thisCell.isRoom()) {
+			numSteps = 1;
+		}
+		makeAdjacencyList(thisCell);
+		HashSet<TestBoardCell> adj = thisCell.getAdjList();
+		for(TestBoardCell c: adj) {
+			if(visited.contains(c)) {
+				continue; // skip this cell
+			} 
+			visited.add(c);
+			if (numSteps == 1) {
+				targets.add(c);
+			} else {
+				findAllTargets(c, numSteps - 1);
+			}
+			visited.remove(c);
+		}
 	}
 	public HashSet<TestBoardCell> getTargets(){
 		return new HashSet<TestBoardCell>();
@@ -21,9 +46,9 @@ public class TestBoard {
 	public TestBoardCell getCell(int row, int col) {
 		return board[row][col];
 	}
-	
+
 	public void makeAdjacencyList(TestBoardCell cell) {
-		
+
 		if(cell.getRow() +1 <= ROWS) {
 			cell.addAdjacency(this.getCell(cell.getRow()+1, cell.getCol()));
 		}
--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -13,13 +13,12 @@ import experiment.TestBoardCell;
 
 class BoardTestsExp {
 	TestBoard test;
-
-	private static final int[][] INTS = new int [4][4];
+	
 	@BeforeEach
 	// TODO: Come back to this (office hours?)
 	// create a test board object to test all other adjacencies on
 	void init() {
-		int [][] arr = INTS;
+		TestBoardCell [][] arr = new TestBoardCell[TestBoard.ROWS][TestBoard.COLS];
 		test = new TestBoard(arr);
 	}
 
@@ -28,8 +27,8 @@ class BoardTestsExp {
 	@Test
 	void testAdjacencyTopLeftCorner() {
 		// Tests adjacency lists on the top left corner
-		TestBoardCell cell = test.getCell(0, 0);
-		HashSet<TestBoardCell> testAdj = cell.getAdjList();
+		TestBoardCell testCell = test.getCell(0, 0);
+		HashSet<TestBoardCell> testAdj = testCell.getAdjList();
 		Assert.assertTrue(testAdj.contains(test.getCell(1, 0)));
 		Assert.assertTrue(testAdj.contains(test.getCell(0, 1)));
 		Assert.assertEquals(2, testAdj.size());
@@ -39,8 +38,8 @@ class BoardTestsExp {
 	@Test
 	void testAdjacencyBottomRightCorner() {
 		// tests adjacency on the bottom right hand corner 
-		TestBoardCell cell = test.getCell(3, 3);
-		HashSet<TestBoardCell> testAdj = cell.getAdjList();
+		TestBoardCell testCell = test.getCell(3, 3);
+		HashSet<TestBoardCell> testAdj = testCell.getAdjList();
 		Assert.assertTrue(testAdj.contains(test.getCell(2, 3)));
 		Assert.assertTrue(testAdj.contains(test.getCell(3, 2)));
 		Assert.assertEquals(2, testAdj.size());
@@ -49,15 +48,15 @@ class BoardTestsExp {
 	@Test
 	void testAdjacencyRightEdge() {
 		// tests adjacency lists on the right edge
-		TestBoardCell cell = test.getCell(1, 3);
+		TestBoardCell testCell = test.getCell(1, 3);
 		TestBoardCell adjacentTop = new TestBoardCell(0, 3);
 		TestBoardCell adjacentBottom = new TestBoardCell(2, 3);
 		TestBoardCell adjacentLeft = new TestBoardCell(1, 2);
 
-		Assert.assertTrue(cell.getAdjList().contains(adjacentTop));
-		Assert.assertTrue(cell.getAdjList().contains(adjacentBottom));
-		Assert.assertTrue(cell.getAdjList().contains(adjacentLeft));
-		Assert.assertEquals(3, cell.getAdjList().size());
+		Assert.assertTrue(testCell.getAdjList().contains(adjacentTop));
+		Assert.assertTrue(testCell.getAdjList().contains(adjacentBottom));
+		Assert.assertTrue(testCell.getAdjList().contains(adjacentLeft));
+		Assert.assertEquals(3, testCell.getAdjList().size());
 
 	}	
 	@Test
@@ -95,8 +94,8 @@ class BoardTestsExp {
 	void testTargets4() {
 		// tests a move of 4 on an empty board
 		//(0, 2), (1,1), (2, 0), (3, 1), (2, 2), (1, 3)
-		TestBoardCell cell = test.getCell(0,0);
-		test.calcTargets(cell, 4);
+		TestBoardCell testCell = test.getCell(0,0);
+		test.calcTargets(testCell, 4);
 		HashSet<TestBoardCell> targets = test.getTargets();
 		Assert.assertEquals(6, targets.size());
 		Assert.assertTrue(targets.contains(test.getCell(0,2)));
@@ -111,8 +110,8 @@ class BoardTestsExp {
 	@Test
 	void testTargets5() {
 
-		TestBoardCell cell = test.getCell(0,0);
-		test.calcTargets(cell, 5);
+		TestBoardCell testCell = test.getCell(0,0);
+		test.calcTargets(testCell, 5);
 		HashSet<TestBoardCell> targets = test.getTargets();
 		Assert.assertEquals(8, targets.size());
 		Assert.assertTrue(targets.contains(test.getCell(0,1)));
@@ -129,8 +128,8 @@ class BoardTestsExp {
 	@Test
 	void testTargets2() {
 
-		TestBoardCell cell = test.getCell(0,0);
-		test.calcTargets(cell, 2);
+		TestBoardCell testCell = test.getCell(0,0);
+		test.calcTargets(testCell, 2);
 		HashSet<TestBoardCell> targets = test.getTargets();
 		Assert.assertEquals(3, targets.size());
 		Assert.assertTrue(targets.contains(test.getCell(1,1)));
@@ -142,8 +141,8 @@ class BoardTestsExp {
 	void testOccupied() {
 		// tests a move of 4 on an empty board
 		//(0, 2), (1,1), (2, 0), (3, 1), (2, 2), (1, 3)
-		TestBoardCell cell = test.getCell(0,0);
-		test.calcTargets(cell, 4);
+		TestBoardCell testCell = test.getCell(0,0);
+		test.calcTargets(testCell, 4);
 		test.getCell(3, 1).setOccupied(true);
 		test.getCell(1, 3).setOccupied(true);
 		HashSet<TestBoardCell> targets = test.getTargets();
@@ -159,8 +158,8 @@ class BoardTestsExp {
 	}
 	@Test
 	void testRoom() {
-		TestBoardCell cell = test.getCell(0,0);
-		test.calcTargets(cell, 4);
+		TestBoardCell testCell = test.getCell(0,0);
+		test.calcTargets(testCell, 4);
 		test.getCell(3, 1).setRoom(true);
 		test.getCell(1, 3).setRoom(true);
 		HashSet<TestBoardCell> targets = test.getTargets();
