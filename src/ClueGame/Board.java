ff8ffc7adeadb77cefa868200acadf11b370fc82
Katherine Aubert <kaubert@mymail.mines.edu>
2021-03-04 10:44:37 -0700

    Started working on load config files, specifically load layout file.

--- src/ClueGame/Board.java
+++ src/ClueGame/Board.java
@@ -1,10 +1,13 @@
 package clueGame;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.HashMap;
 
 import experiment.TestBoardCell;
 
 public class Board {
+	// read the files and set numrows and numcols
 	private int numRows = 0;
 	private int numCols = 0;
 	BoardCell [][] grid = new BoardCell [numRows][numCols];
@@ -14,9 +17,22 @@ public class Board {
 	
 	private static Board theInstance = new Board();
     // constructor is private to ensure only one can be created
-    private Board() {
-           super();
-    }
+	private Board() {
+		super();
+		initialize();
+		grid = new TestBoardCell[numRows][numCols];
+		for(int row = 0; row < numRows; row ++) {
+			for(int col = 0; col < numCols; col ++) {
+				grid[row][col] = new TestBoardCell(row, col);
+			}
+		}
+		for(int row = 0; row < numRows; row ++) {
+			for(int col = 0; col < numCols; col ++) {
+				makeAdjacencyList(grid[row][col]);
+			}
+		}
+	}
+    public void
     // this method returns the only Board
     public static Board getInstance() {
            return theInstance;
@@ -25,17 +41,54 @@ public class Board {
      * initialize the board (since we are using singleton pattern)
      */
     public void initialize() {
-    	return;
+    	loadConfigFiles();
     }
     public void loadConfigFiles() {
-    	
+    	loadLayoutConfig();
+    	loadSetupConfig();
     }
     public void loadSetupConfig() {
+    	// read setup file and set relevant variables
     	
     }
     public void loadLayoutConfig() {
+    	// load layout file and set relevant variables
+    	// while has next line:
+    	// figure out how many columns there are
+    	// at the end, figure out how many rows there are
     	
+    	BufferedReader br = new BufferedReader(new FileReader(layoutConfigFile));
+    	int rowCount = 1;
+    	String line = br.readLine();
+    	String[] arr = line.split(",");
+    	int colCount = arr.length;
+    	while (line != null) {
+    		rowCount++;
+    		// have a counter variable for columns
+    		arr = line.split(",");
+    		
+    		if (arr.length != colCount) {
+    			throw new BadConfigFormatException();
+    		}
+    		line = br.readLine();
+
+    	}
+    	numRows = rowCount;
+    	numCols = colCount;
+    	String[][] grid2 = new String[numRows][numCols];
+    	br = new BufferedReader(new FileReader(layoutConfigFile));
+    	for(int i = 0; i < numRows; i++) {
+    		line = br.readLine();
+    		arr = line.split(",");
+    		for(int column = 0; column < arr.length; column ++) {
+    			BoardCell c = new BoardCell(i, column);
+    			// TODO: add appropriate setters
+    		}
+    	}
     }
+    	// check if all of the rows are the same length
+
+    
 	public Room getRoom(char initial) {
 		Room charRoom = new Room();
 		return charRoom;
@@ -45,9 +98,15 @@ public class Board {
 		return cellRoom;
 	}
 	public void setConfigFiles(String layout, String setup) {
-		return;
+		layoutConfigFile = layout;
+		setupConfigFile = setup;
 	}
 	public int getNumRows() {
+		//numRows = -1
+		if (numRows != -1) {
+			return numRows
+		}
+		
 		return numRows;
 	}
 	public int getNumColumns() {
--- src/ClueGame/BoardCell.java
+++ src/ClueGame/BoardCell.java
@@ -7,9 +7,33 @@ public class BoardCell {
 	private int row = 0;
 	private int col = 0;
 	private HashSet<BoardCell> adjacencyList = new HashSet<BoardCell>();
-	private char initial;
+	private String initial;
 	private boolean roomLabel;
 	private boolean roomCenter;
+	public String getInitial() {
+		return initial;
+	}
+	public void setInitial(String initial) {
+		this.initial = initial;
+	}
+	public boolean isRoomLabel() {
+		return roomLabel;
+	}
+	public void setRoomLabel(boolean roomLabel) {
+		this.roomLabel = roomLabel;
+	}
+	public DoorDirection getDoordirection() {
+		return doordirection;
+	}
+	public void setDoordirection(DoorDirection doordirection) {
+		this.doordirection = doordirection;
+	}
+	public void setRoomCenter(boolean roomCenter) {
+		this.roomCenter = roomCenter;
+	}
+	public void setSecretPassage(char secretPassage) {
+		this.secretPassage = secretPassage;
+	}
 	private char secretPassage = 'a';
 	private DoorDirection doordirection;
 	public BoardCell(int row, int col) {
